539056f0525f15a8d8167225b44d7c7bb0d6eb8b
Danny_Nguyen <dannyhn.it@gmail.com>
2020-08-27 10:05:45 +0700

    update variables and test commit user.name + user.email

--- utility/create-connector.sh
+++ utility/create-connector.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-
+configuration_file_name=$1
 echo "POST connector: "
 echo
 curl -i -X POST -H "Accept:application/json" \
         -H  "Content-Type:application/json" localhost:8083/connectors \
-        -d @sink_connector.json
+        -d @$configuration_file_name.json
 echo
